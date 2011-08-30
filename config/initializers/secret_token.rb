59881261c331d02203244ac3d072c70ca56ddcf0
Zbyszek Sobczyński <zbyszek.sobczynski@me.com>
2011-08-30 22:00:11 +0200

    Folder testowy na aplicje - Aptana Commit
    Test Master Branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyDEV::Application.config.secret_token = '07218c30ec620dd57ae78d7c4fc527fcae27a36cfb4b6e978531b424b43568e1c3322184f91d4515dae1c2eed91f9fb1c275457ebd36ceaf1938d8c26f76eb51'
